4308801fb43f63b7a4eab1b979940798e7771c48
Aurel Lazar <lazar.aurel@gmail.com>
2017-03-02 20:32:17 -0800

    Create the User in the appropriate Authentication Middleware out of headers passed in by Azure Authentication/Authorization Service by cracking the JWT token passed in by the Easy-Auth module.

--- UpendService/Middleware/AuthInjector.cs
+++ UpendService/Middleware/AuthInjector.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace UpendService.Middleware
 {
-    public class AuthInjector
-    {
+	public class AuthInjector
+	{
 		private readonly RequestDelegate next;
 		private readonly IHostingEnvironment env;
 
@@ -23,17 +25,46 @@ namespace UpendService.Middleware
 		{
 			if(env.IsDevelopment())
 			{
-				var identity = new ClaimsIdentity();
-				var claims = new Claim[]
-				{
+				CreateDevelopmentUser(context);
+			}
+			else
+			{
+				CreateProductionUser(context);
+			}
+			await next(context);
+		}
+
+		private static void CreateProductionUser(HttpContext context)
+		{
+			if (context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"] == StringValues.Empty)
+				return;
+			
+			var idp = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-IDP"];
+			var idHeader = "X-MS-TOKEN-" + idp.ToString().ToUpperInvariant() + "-ID-TOKEN";
+
+			if (context.Request.Headers[idHeader] == StringValues.Empty)
+				return;
+
+			JwtSecurityToken token = new JwtSecurityToken(context.Request.Headers[idHeader]);
+			var identity = new ClaimsIdentity(token.Claims, "Automatic");
+
+			// Add a few custom claims to the identity that we use internally
+			identity.AddClaim(new Claim("http://schemas.microsoft.com/identity/claims/identityprovider", context.Request.Headers["X-MS-CLIENT-PRINCIPAL-IDP"]));
+			identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"]));
+
+			context.User = new ClaimsPrincipal(identity);
+		}
+
+		private static void CreateDevelopmentUser(HttpContext context)
+		{
+			var identity = new ClaimsIdentity();
+			var claims = new Claim[]
+			{
 					new Claim("http://schemas.microsoft.com/identity/claims/identityprovider", "fake"),
 					new Claim(ClaimTypes.NameIdentifier, "testuser")
-				};
-				identity.AddClaims(claims);
-				context.User = new ClaimsPrincipal(identity);
-			}
-
-			await next(context);
+			};
+			identity.AddClaims(claims);
+			context.User = new ClaimsPrincipal(identity);
 		}
 	}
 }
