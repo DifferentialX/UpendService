fed1435b84a63cf66b6dbd989d49c658c3df2954
Aurel Lazar <lazar.aurel@gmail.com>
2017-05-11 21:27:01 -0700

    Create a Controller Factory that allows us to easily generate the objects we need on the fly in our test in a generic way.

---
+++ UpendServiceTest/Helpers/ControllerFactory.cs
@@ -0,0 +1,29 @@
+﻿using UpendService;
+using UpendService.Controllers;
+using UpendService.Models;
+using UpendService.Services;
+
+namespace UpendServiceTest
+{
+	internal static class ControllerFactory
+	{
+		public static BaseController<T> Create<T>(ModelContext context, ICurrentIdentity identity) 
+			where T : Data<T> 
+		{
+			var type = typeof(T);
+			if (type == typeof(Task))
+			{
+				return new TaskController(context, identity) as BaseController<T>;
+			}
+			else if (type == typeof(Action))
+			{
+				return new ActionController(context, identity) as BaseController<T>;
+			}
+			else if (type == typeof(User))
+			{
+				return new UserController(context, identity) as BaseController<T>;
+			}
+			throw new System.Exception("Should never be hit.");
+		}
+	}
+}
